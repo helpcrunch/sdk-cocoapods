d9456289d4656c648a01a41404947aa8f06d5266
Andrii Gusarov <fishkillerru@gmail.com>
2018-10-09 21:09:50 +0300

    Testing with m file

---
+++ HC_Version.h
@@ -0,0 +1,16 @@
+//
+//  HC_Version.h
+//  Pods-HelpCrunchSDK_Tests
+//
+//  Created by Andrii Gusarov on 10/9/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HC_Version : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HC_Version.m
@@ -0,0 +1,12 @@
+//
+//  HC_Version.m
+//  Pods-HelpCrunchSDK_Tests
+//
+//  Created by Andrii Gusarov on 10/9/18.
+//
+
+#import "HC_Version.h"
+
+@implementation HC_Version
+
+@end
