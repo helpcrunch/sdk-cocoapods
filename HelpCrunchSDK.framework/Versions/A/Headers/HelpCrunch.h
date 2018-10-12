203f3f34f8d8fa3a1e510468fb508906ce609999
Andrii Gusarov <fishkillerru@gmail.com>
2018-10-12 20:29:51 +0300

    Trying swift version of version

--- HelpCrunchSDK.framework/Versions/A/Headers/HelpCrunch.h
+++ HelpCrunchSDK.framework/Versions/A/Headers/HelpCrunch.h
@@ -22,7 +22,7 @@ FOUNDATION_EXTERN NSString *const HC_CustomDataAttributeName;
 
 FOUNDATION_EXTERN NSString *const HC_OpenLinkNotification;
 
-static NSString *const HC_Version = @"1.1.00";
+static NSString *const HC_SDKVersion = @"1.1.00";
 
 /**
  Fires when user press 'Done' button on the chat view controller
---
+++ HelpCrunchVersion.swift
@@ -0,0 +1,11 @@
+//
+//  HelpCrunchVersion.swift
+//  HelpCrunchSDK
+//
+//  Created by HelpCrunch on 10/12/18.
+//  Copyright © 2018 HelpCrunch. All rights reserved.
+//
+
+import Foundation
+
+static let HelpCrunchSDKVersion = "1.1.00"
