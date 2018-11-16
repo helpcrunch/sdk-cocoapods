13349a25b4af45394c8829f161b51651593b3dd9
Andrii Gusarov <fishkillerru@gmail.com>
2018-11-16 16:37:09 +0200

    Removed description from swift file

--- HelpCrunchVersion.swift
+++ HelpCrunchVersion.swift
@@ -1,11 +1,3 @@
-//
-//  HelpCrunchVersion.swift
-//  HelpCrunchSDK
-//
-//  Created by HelpCrunch on 10/12/18.
-//  Copyright © 2018 HelpCrunch. All rights reserved.
-//
-
 import Foundation
 
 extension HelpCrunch {
